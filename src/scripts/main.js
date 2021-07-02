8ffcc4d8fbaef38bc0f728deeb6afdbd18b02098
cnoss <christian.noss@th-koeln.de>
2021-07-02 11:52:44 +0200

    refactor(JS): Javascript verwendet jetzt Klassen

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,70 +1,91 @@
-const setNavState = () => {
+class Navigation {
 
-  const slug = window.location.pathname;
-  const navItems = document.querySelectorAll('[data-js-navitem]');
+  constructor() {
+    this.slug = window.location.pathname;
+    this.navItems = document.querySelectorAll('[data-js-navitem]');
+  }
+
+  showActiveSubmenu() {
+    this.navItems.forEach(item => {
+      const anchor = item.querySelector('[href]');
+      
+      if (!anchor.href.match(this.slug)) return;
+      item.setAttribute("data-state", "active");
   
-  navItems.forEach(item => {
-    const anchor = item.querySelector('[href]');
-    console.log(slug, anchor);
-    if (!anchor.href.match(slug)) return;
-    item.setAttribute("data-state", "active");
-
-    if (!item.querySelector('.subnavbar')) return;
-    item.querySelector('.subnavbar').setAttribute("data-state", "active");
-    
-  });
-  return;
+      if (!item.querySelector('.subnavbar')) return;
+      item.querySelector('.subnavbar').setAttribute("data-state", "active");
+      
+    });
+  }  
 }
 
-const initDropdowns = () => {
-  const trigger = document.querySelectorAll('[data-js-dropdown]');
-  trigger.forEach(
-    ele => {
-      ele.addEventListener('click', (event) => {
-        const dropDownTargetName = event.currentTarget.dataset.jsDropdown;
-        const dropDownTarget = document.querySelector(`[data-js-dropdown-target=${dropDownTargetName}]`);
+class Dropdown {
+  constructor() {
+    this.trigger = document.querySelectorAll('[data-js-dropdown]');
+    this.init();
+  }
 
-        if (dropDownTarget.dataset.state === 'visible') {
-          dropDownTarget.dataset.state = 'closed';
-          ele.dataset.state = 'closed';
-        } else {
-          dropDownTarget.dataset.state = 'active';
-          ele.dataset.state = 'active';
+  init() {
+    this.trigger.forEach(
+      ele => {
+        ele.addEventListener('click', (event) => {
+          const dropDownTargetName = event.currentTarget.dataset.jsDropdown;
+          const dropDownTarget = document.querySelector(`[data-js-dropdown-target=${dropDownTargetName}]`);
+  
+          if (dropDownTarget.dataset.state === 'active') {
+            dropDownTarget.dataset.state = 'closed';
+            ele.dataset.state = 'closed';
+          } else {
+            dropDownTarget.dataset.state = 'active';
+            ele.dataset.state = 'active';
+          }
+        });
+      }
+    );
+  }
+}
+
+class ScrollSpy {
+
+  constructor(menu, sections) {
+    this.menu = menu;
+    this.sections = sections;
+    this.lastActiveItem = false;
+
+    const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) =>{
+        if (entry.isIntersecting) {
+          this.refreshNavigation(entry);
         }
       });
-    }
-  );
-}
-
-const initScrollSpy = () => {
-
-  let lastActiveItem = false;
-  const menu = document.querySelector('.subnavbar');
-  const sections = document.querySelectorAll('[data-js-scrollspy]');
-
-  const imageObserver = new IntersectionObserver ((entries, observer)=>{
-    entries.forEach(function(entry) {
-      if (entry.isIntersecting) {
-        const id = entry.target.id;
-        const pattern = `[href$=${id}]`;
-        const anchor = menu.querySelector(pattern);
-        const menuItem = anchor.closest('.subnavbar__item');
-
-        if (lastActiveItem) { lastActiveItem.removeAttribute("data-state"); }
-        menuItem.setAttribute("data-state", "active");
-        lastActiveItem = menuItem;
-      }
     });
-  });
-  
-  sections.forEach(function(section) {
-    imageObserver.observe(section);
-  });
-}
+    
+    this.sections.forEach(function(section) {
+      imageObserver.observe(section);
+    });
+  }
 
+  refreshNavigation(targetNavElement) {
+    const id = targetNavElement.target.id;
+    const pattern = `[href$=${id}]`;
+    const anchor = this.menu.querySelector(pattern);
+    const menuItem = anchor.closest('.subnavbar__item');
+
+    if (this.lastActiveItem) { this.lastActiveItem.removeAttribute("data-state"); }
+    menuItem.setAttribute("data-state", "active");
+    this.lastActiveItem = menuItem;
+  }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  initDropdowns();
-  setNavState();
-  initScrollSpy();
+
+  const navigation = new Navigation();
+  navigation.showActiveSubmenu();
+
+  new Dropdown();
+
+  const menu = document.querySelector('.subnavbar');
+  const sections = document.querySelectorAll('[data-js-scrollspy]');
+  new ScrollSpy(menu, sections);
+  
 });
