d04176670fbb05df7764b5dd5211289f43bc3fcb
cnoss <christian.noss@th-koeln.de>
2021-07-04 19:09:01 +0200

    Static Page Generator 11ty integriert
    Hierfür sind einige Umbauten erforderlich gewesen und die BuildChain
    wurde auch angepasst:
    - `dist` Folder wurde zu `docs` Folder, damit man das Ganze später auch
      mal auf GitHub Pages laufen lassen kann.
    - `scripts` und `styles` sind in den `assets` Folder gewandert, damit
      das Wurzelverzeichnis etwas übersichtlicher ist.
    - Es wurde ein default-Template erzeugt.
    - Die Navigation wurde in einer Komponente ausgelagert.

--- docs/assets/scripts/main.js
+++ docs/assets/scripts/main.js
@@ -3,18 +3,53 @@ class Navigation {
   constructor() {
     this.slug = window.location.pathname;
     this.navItems = document.querySelectorAll('[data-js-navitem]');
+    this.submenu = false;
+    this.currentNavElement = false;
   }
 
-  init() {
+  createSubmenu() {
+    const elements = document.querySelectorAll('[data-js-scrollspy]');
+    const submenuItems = [];
+    elements.forEach((element) => {
+      const link = `#${element.id}`;
+      const linktext = element.querySelector('[data-js-linktext]').innerText;
+      const item = `
+        <li class="subnavbar__item">
+          <a href="${link}">${linktext}</a>
+        </li>
+      `;
+
+      submenuItems.push(item);
+    });
+
+    const submenu = `
+      <ul class="subnavbar" data-js-dropdown-target="theory">
+        ${submenuItems.join('\n')}
+      </ul>
+    `;
+
+    this.submenu = submenu;
+  }
+
+  getCurrentPage() {
     this.navItems.forEach((item) => {
       const anchor = item.querySelector('[href]');
 
       if (!anchor.href.match(this.slug)) return;
       item.setAttribute('data-state', 'active');
 
+      const wrapper = document.createElement('div');
+      wrapper.innerHTML = this.submenu;
+      item.appendChild(wrapper);
+
       if (!item.querySelector('.subnavbar')) return;
       item.querySelector('.subnavbar').setAttribute('data-state', 'active');
     });
+  }
+
+  init() {
+    this.createSubmenu();
+    this.getCurrentPage();
   }
 }
 
@@ -84,18 +119,20 @@ class Comments {
   }
 
   init() {
-    fetch(this.url)
-      .then((data) => data.json())
-      .then((data) => {
-        this.renderData(data);
-      })
-      .catch((error) => {
-        this.renderError(error);
-      });
+    if (this.target) {
+      fetch(this.url)
+        .then((data) => data.json())
+        .then((data) => {
+          this.renderData(data);
+        })
+        .catch((error) => {
+          this.renderError(error);
+        });
+    }
   }
 
   renderError(error) {
-    this.target.innerHTML = `<li>Die Kommentare konnten leider nicht geladen werden. Fehler: ${error}</li>`;
+    this.target.innerHTML = `<li>Die Kommentare konnten leider nicht geladen werden.  Fehler: ${error}</li>`;
   }
 
   renderData(data) {
